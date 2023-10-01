5ef7057c2257165dbd415bd7f96620e4efb9fa15
Gian <epilred@icloud.com>
2023-10-01 23:56:50 +0800

    removed build directory, gitignore should prevent it from gettig committed, initial commit, macOS < catalina not compatible, have to switch workstations

--- build/lib/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/dev/projects/vulkan-demo/lib/glm/glm
-# Build directory: D:/dev/projects/vulkan-demo/build/lib/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
